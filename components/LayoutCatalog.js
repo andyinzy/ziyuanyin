6798be84da3f9bc17fc64cc445bfd5331bc482f6
ziyuanyin <ziyuan_yin@foxmail.com>
2023-06-01 16:09:27 +0800

    perf tuned

--- components/LayoutCatalog.js
+++ components/LayoutCatalog.js
@@ -95,7 +95,7 @@ export default function LayoutCatalog ({ blockMap }) {
             </div>
           </div>
     {/* <aside className='font-serif flex-auto ml-4 md:ml-12 mt-10 pl-2 md:pl-5 md:border-l-2 border-gray-200 text-sm text-zinc-700/70 dark:text-neutral-400 select-none' style={style}> */}
-          <div className='overflow-y-auto max-h-52 lg:max-h-96 overscroll-none scroll-smooth scroll-py-4' ref={tRef}>
+          <div className='overflow-y-auto max-h-52 lg:max-h-96 overscroll-none scroll-smooth scroll-py-4 scroll-hidden' ref={tRef}>
             <nav className='h-full text-black dark:text-gray-300'>
                 {/* {nodes.map(node => (
                   <div key={node.id}>

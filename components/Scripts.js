a3fe2727af498a2e7dac98d6bb22697dde9353e1
andyzy <ziyuan_yin@foxmail.com>
2024-09-06 23:05:36 +0800

    rmv lazyload in renderer

--- components/Scripts.js
+++ components/Scripts.js
@@ -1,27 +1,10 @@
 import Script from 'next/script'
 import { useConfig } from '@/lib/config'
-import { loadExternalResource } from '@/lib/utils';
 import {useEffect} from 'react'
 
 const Scripts = () => {
   const BLOG = useConfig()
-  useEffect(() => {
-    // 懒加载 CSS 文件
-    loadExternalResource('https://example.com/styles.css', 'css')
-      .then(() => console.log('CSS loaded'))
-      .catch(err => console.error('Error loading CSS:', err));
 
-    // 懒加载外部 JS 文件
-    loadExternalResource('https://example.com/extra-script.js', 'js')
-      .then(() => console.log('JS script loaded'))
-      .catch(err => console.error('Error loading JS:', err));
-
-    // 懒加载字体
-    loadExternalResource('https://example.com/font.css', 'font')
-      .then(() => console.log('Font loaded'))
-      .catch(err => console.error('Error loading font:', err));
-  }, []); // 空数组表示只在组件挂载时运行一次
-  
   return (
     <>
       {BLOG.analytics && BLOG.analytics.provider === 'ackee' && (

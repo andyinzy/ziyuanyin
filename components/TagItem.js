bf1c696103343dadd653f5fe186eed5c53ba55e9
andyzy <ziyuan_yin@foxmail.com>
2024-09-08 15:32:31 +0800

    shadow tuned

--- components/TagItem.js
+++ components/TagItem.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const TagItem = ({ tag }) => (
   <Link href={`/tag/${encodeURIComponent(tag)}`}>
-    <p className="mr-1 rounded-xl px-2 py-1 custom-page-tag-hover-shadow border leading-none text-sm dark:border-gray-600">
+    <p className="mx-1 rounded-xl px-2 py-1 custom-page-tag-hover-shadow border leading-none text-sm dark:border-gray-600">
       {tag}
     </p>
   </Link>
